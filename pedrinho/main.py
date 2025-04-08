b3aab473068c43108eeb0392ca093c2191dc8468
PedroLuaks <pedro.firmo.silva@escola.pr.gov.br>
2025-04-08 17:53:08 -0300

    Update main.py

--- pedrinho/main.py
+++ pedrinho/main.py
@@ -1,57 +1,54 @@
 import json
 
-arquivo_cadastros="cadastros.json"
-
 def exibir_menu():
-    print("Bem vindo ao menu de cadastro!")
-    print("1- Novo cadastro")
-    print("2- Ver cadastro")
-    print("3- Sair")
-    print("------------------------------")
-
-def salvar_cadastros (cadastros):
-    with open (arquivo_cadastros, "w", encoding="utf-8") as arquivo:
-        json.dump(cadastros, arquivo, indent=4, ensure_asscii=False)    
+    print("\n====== MENU CADASTRO ========")
+    print("1. Cadastrar pessoa")
+    print("2. Ver cadastros")
+    print("3. Sair")
+    print("===========================")
 
 def carregar_cadastros():
     try:
-        with open (arquivo_cadastros,"r",encoding="utf-8") as arquivo:
+        with open("cadastros.json", "r") as arquivo:
             return json.load(arquivo)
-    except(FileNotFoundError,json.JSONDecodeError):
-        return[]   
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
- 
+def salvar_cadastros(cadastros):
+    with open("cadastros.json", "w") as arquivo:
+        json.dump(cadastros, arquivo, indent=4)
+
 def cadastrar_pessoa(cadastros):
-    nome = input("nome: ")
-    idade = int(input("idade: "))
-    turma = input("Turma: ")
-    curso = input("curso: ")
-    cadastros.append({"nome": nome, "idade": idade, "turma": turma, "curso": curso})
+    nome = input("Nome:")
+    idade = input("Idade:")
+    turma = input("Turma:")
+    curso = input("Curso:")
+    cadastros.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso})
+    salvar_cadastros(cadastros)
     print("Cadastro realizado com sucesso!")
 
-def ver_cadastro(cadastros):
+def ver_cadastros(cadastros):
     if not cadastros:
-        print("Nenhum cadastro no sistema")
+        print("\Nenhum cadastro realizado.")
     else:
-        print("\n------LISTA DE CADASTROS------")
+        print("\n=== LISTA DE CADASTROS ===")
         for i, pessoa in enumerate(cadastros, 1):
-            print(f"{i}. nome: {pessoa['nome']}, idade: {pessoa['idade']}, turma: {pessoa['turma']}, curso: {pessoa['curso']}")
+            print(f"{i}. Nome: {pessoa['Nome']}, Idade: {pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
 
 def main():
-    cadastros = []
+    cadastros = carregar_cadastros()
     while True:
         exibir_menu()
-        opção = input("Escolha uma opção: ")
-        if opção == "1":
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
             cadastrar_pessoa(cadastros)
-        elif opção == "2":
-            ver_cadastro(cadastros)
-        elif opção == "3":
-            print("Obrigado por utilizar o sistema!")
+        elif opcao == "2":
+            ver_cadastros(cadastros)
+        elif opcao == "3":
+            print("Obrigado por utilizar o sistema de cadastro!")
             break
         else:
-            print("Opção incorreta, tente novamente")
+            print("Opção inválida! Tente novamente.")
 
 if __name__ == "__main__":
     main()
-     
